202d877105d81d3936f772ef645e72db5d10e53a
lulu <lulu2184@gmail.com>
2014-11-14 22:26:17 +0800

    step3 start

---
+++ SimilarVideoRecognizer/GroupOfHashValueVector.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "GroupOfHashValueVector.h"
+
+
+GroupOfHashValueVector::GroupOfHashValueVector()
+{
+}
+
+
+GroupOfHashValueVector::~GroupOfHashValueVector()
+{
+}
---
+++ SimilarVideoRecognizer/GroupOfHashValueVector.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <vector>
+
+class GroupOfHashValueVector
+{
+private:
+	std::vector<std::vector<int>> m_HashValue;
+
+public:
+	GroupOfHashValueVector();
+	~GroupOfHashValueVector();
+
+};
+
